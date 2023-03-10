4afef18d8b61d35f43923455965f393d7d0c80c0
Gönenç Demirkaan <80961341+gonencdemirkaan@users.noreply.github.com>
2023-03-10 15:32:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+# 1.Hafta 1.Gün Ödev
+# veri Tipleri
+
+a = "Gönenç" #String
+b = 1 #İnteger
+c = 1.5 # Float
+d = True # Boolean
+
+
+departmanlar = ["Enerji","Bilişim","Bakım","İnsan Kaynakları"] # Lists
+mühendisler = ("Elektrik-Elektronik Müh.","Bilişim Sistemleri Müh.","Makina Müh.","Çevre Müh") # Touples
+calisanlar = {"Gönenç": "0555 666 77 88","Ali":"0555 111 22 33","Veli":"0555 444 55 00"} # Dictionaries
+
+# Sitede değişken olarak düşündüğüm veeriler
+
+    #Üye isimleri,üye e-postaları,adres,şifre ==> String
+    #bitirme oranı ==> float
+    
+
+#Şart blokları örnekleri
+
+ders1 = False
+ders2 = False
+ders3 = False
+ders4 = False
+bitirme_orani = 0.0
+
+if ders1 == True:
+    bitirme_orani + 25.0
+    print(bitirme_orani)
+else: 
+    print(bitirme_orani) 
+
+if ders2 == True:
+    bitirme_orani + 25.0
+    print(bitirme_orani)
+else: 
+    print(bitirme_orani)  
+   
+if ders3 == True:
+    bitirme_orani + 25.0
+    print(bitirme_orani)
+else: 
+    print(bitirme_orani)  
+
+if ders4 == True:
+    bitirme_orani + 25.0
+    print(bitirme_orani)
+else: 
+    print(bitirme_orani)    
